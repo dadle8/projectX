80c2b14e5a8d06d5581412f82f5d969c4607fc03
Vyacheslav Tikhonov <dadle8@yandex.ru>
2016-11-03 23:40:18 +0300

    Fixed projectx.sql

--- database/projectx.sql
+++ database/projectx.sql
@@ -3,35 +3,35 @@ use projectx;
 
 create table person 
 (
-	id_person INT auto_increment,
+    idperson INT auto_increment,
     name varchar(45) not null,
     surname varchar(45) not null,
     login varchar(45) not null,
     password varchar(45) not null,
     lastdatelogin DATE,
-    is_online INT,
-	primary key(id_person)
+    isonline INT,
+    primary key(idperson)
 );
 
 create table geolocations
 (
-	id_geolocation INT auto_increment,
-	geolocation geometry not null,
+    idgeolocation INT auto_increment,
+    geolocation geometry not null,
     date_geolocation date not null,
-    id_person INT,
-    primary key(id_geolocation),
-    FOREIGN KEY (id_person) REFERENCES person (id_person)
+    idperson INT,
+    primary key(idgeolocation),
+    FOREIGN KEY (idperson) REFERENCES person (idperson)
 );
 
 create table messages
 (
-	id_message INT auto_increment,
+    idmessage INT auto_increment,
     message VARCHAR(1024) not null,
-    date_message DATE not null,
-    is_read INT not null,
-    id_from INT,
-    id_to INT,
-    primary key(id_message),
-    FOREIGN KEY (id_from) REFERENCES person (id_person),
-    FOREIGN KEY (id_to) REFERENCES person (id_person)
+    datemessage DATE not null,
+    isread INT not null,
+    idfrom INT,
+    idto INT,
+    primary key(idmessage),
+    FOREIGN KEY (idfrom) REFERENCES person (idperson),
+    FOREIGN KEY (idto) REFERENCES person (idperson)
 );
