55ac92e21fec68b2d2ba97d1421355c700e29465
NullHorizon <Outcaster@mail.ru>
2016-11-17 01:44:30 +0300

    new sql

--- database/projectx_old.sql
+++ database/projectx_old.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Хост: 127.0.0.1
--- Время создания: Ноя 16 2016 г., 22:42
+-- Время создания: Ноя 08 2016 г., 17:58
 -- Версия сервера: 5.7.11
 -- Версия PHP: 5.6.19
 
@@ -23,26 +23,16 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Структура таблицы `geo`
+-- Структура таблицы `geolocations`
 --
 
-CREATE TABLE `geo` (
+CREATE TABLE `geolocations` (
   `id` int(11) NOT NULL,
-  `latitude` double DEFAULT NULL,
-  `longitude` double DEFAULT NULL,
-  `userid` int(11) DEFAULT NULL,
-  `time` datetime DEFAULT NULL,
-  `device` varchar(128) DEFAULT NULL
+  `geolocationX` float(10,6) NOT NULL,
+  `geolocationY` float(10,6) NOT NULL,
+  `dategeolocation` date NOT NULL,
+  `iduser` int(11) DEFAULT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
-
---
--- Дамп данных таблицы `geo`
---
-
-INSERT INTO `geo` (`id`, `latitude`, `longitude`, `userid`, `time`, `device`) VALUES
-(1, 59.851199, 30.321108, 1, '2016-11-17 00:15:58', 'PC'),
-(2, 59.853382, 30.321091, 1, '2016-11-17 00:16:55', 'PC'),
-(3, 59.85352, 30.329599, 1, '2016-11-17 00:17:21', 'PC');
 
 -- --------------------------------------------------------
 
@@ -53,7 +43,7 @@ INSERT INTO `geo` (`id`, `latitude`, `longitude`, `userid`, `time`, `device`) VA
 CREATE TABLE `messages` (
   `id` int(11) NOT NULL,
   `message` varchar(1024) NOT NULL,
-  `datemessage` datetime NOT NULL,
+  `datemessage` date NOT NULL,
   `isread` tinyint(1) NOT NULL,
   `idfrom` int(11) NOT NULL,
   `idto` int(11) NOT NULL
@@ -64,15 +54,15 @@ CREATE TABLE `messages` (
 --
 
 INSERT INTO `messages` (`id`, `message`, `datemessage`, `isread`, `idfrom`, `idto`) VALUES
-(1, 'xxx', '2016-11-08 00:00:00', 0, 1, 2),
-(2, 'xxx', '2016-11-08 00:00:00', 0, 1, 2),
-(3, 'xxx', '2016-11-08 00:00:00', 0, 1, 2),
-(4, 'xxx', '2016-11-08 00:00:00', 1, 2, 1),
-(5, 'xxx', '2016-11-08 00:00:00', 1, 2, 1),
-(6, 'xxxqwd', '2016-11-08 00:00:00', 1, 2, 1),
-(7, 'xxx', '2016-11-08 00:00:00', 0, 1, 2),
-(8, 'aaa', '2016-11-08 00:00:00', 1, 2, 1),
-(9, 'bbb', '2016-11-08 00:00:00', 0, 1, 2);
+(1, 'xxx', '2016-11-08', 0, 1, 2),
+(2, 'xxx', '2016-11-08', 0, 1, 2),
+(3, 'xxx', '2016-11-08', 0, 1, 2),
+(4, 'xxx', '2016-11-08', 0, 2, 1),
+(5, 'xxx', '2016-11-08', 0, 2, 1),
+(6, 'xxxqwd', '2016-11-08', 0, 2, 1),
+(7, 'xxx', '2016-11-08', 0, 1, 2),
+(8, 'aaa', '2016-11-08', 0, 2, 1),
+(9, 'bbb', '2016-11-08', 0, 1, 2);
 
 -- --------------------------------------------------------
 
@@ -97,25 +87,16 @@ INSERT INTO `user` (`id`, `login`, `password`, `email`, `loggedIn`, `sessionId`)
 (1, 'root', '63a9f0ea7bb98050796b649e85481845', 'root@root.root', 0, NULL),
 (2, 'admin', '63a9f0ea7bb98050796b649e85481845', 'admin@root.admin', 0, NULL);
 
--- --------------------------------------------------------
-
---
--- Структура таблицы `xxx`
---
-
-CREATE TABLE `xxx` (
-  `xxx` datetime NOT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
-
 --
 -- Индексы сохранённых таблиц
 --
 
 --
--- Индексы таблицы `geo`
+-- Индексы таблицы `geolocations`
 --
-ALTER TABLE `geo`
-  ADD PRIMARY KEY (`id`);
+ALTER TABLE `geolocations`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `iduser` (`iduser`);
 
 --
 -- Индексы таблицы `messages`
@@ -136,10 +117,10 @@ ALTER TABLE `user`
 --
 
 --
--- AUTO_INCREMENT для таблицы `geo`
+-- AUTO_INCREMENT для таблицы `geolocations`
 --
-ALTER TABLE `geo`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+ALTER TABLE `geolocations`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 --
 -- AUTO_INCREMENT для таблицы `messages`
 --
