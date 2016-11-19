732140621846370813c9c466d4505ae97e8c32a6
NullHorizon <Outcaster@mail.ru>
2016-11-19 03:49:03 +0300

    new sql

--- database/projectx.sql
+++ database/projectx.sql
@@ -1,171 +1,170 @@
--- phpMyAdmin SQL Dump
--- version 4.5.5.1
--- http://www.phpmyadmin.net
+-- MySQL dump 10.13  Distrib 5.7.11, for Win64 (x86_64)
 --
--- Хост: 127.0.0.1
--- Время создания: Ноя 16 2016 г., 22:42
--- Версия сервера: 5.7.11
--- Версия PHP: 5.6.19
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+00:00";
-
+-- Host: localhost    Database: projectx
+-- ------------------------------------------------------
+-- Server version	5.7.11
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+03:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- База данных: `projectx`
+-- Current Database: `projectx`
 --
 
--- --------------------------------------------------------
+CREATE DATABASE /*!32312 IF NOT EXISTS*/ `projectx` /*!40100 DEFAULT CHARACTER SET latin1 */;
+
+USE `projectx`;
 
 --
--- Структура таблицы `geo`
+-- Table structure for table `friend`
 --
 
+DROP TABLE IF EXISTS `friend`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `friend` (
+  `id` int(11) DEFAULT NULL,
+  `userId` int(6) unsigned DEFAULT NULL,
+  `friendId` int(6) unsigned DEFAULT NULL,
+  `accepted` tinyint(1) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `friend`
+--
+
+LOCK TABLES `friend` WRITE;
+/*!40000 ALTER TABLE `friend` DISABLE KEYS */;
+INSERT INTO `friend` VALUES (0,1,2,1),(0,1,3,1);
+/*!40000 ALTER TABLE `friend` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `geo`
+--
+
+DROP TABLE IF EXISTS `geo`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `geo` (
-  `id` int(11) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `latitude` double DEFAULT NULL,
   `longitude` double DEFAULT NULL,
   `userid` int(11) DEFAULT NULL,
   `time` datetime DEFAULT NULL,
-  `device` varchar(128) DEFAULT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+  `device` varchar(128) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Дамп данных таблицы `geo`
+-- Dumping data for table `geo`
 --
 
-INSERT INTO `geo` (`id`, `latitude`, `longitude`, `userid`, `time`, `device`) VALUES
-(1, 59.851199, 30.321108, 1, '2016-11-17 00:15:58', 'PC'),
-(2, 59.853382, 30.321091, 1, '2016-11-17 00:16:55', 'PC'),
-(3, 59.85352, 30.329599, 1, '2016-11-17 00:17:21', 'PC');
-
--- --------------------------------------------------------
+LOCK TABLES `geo` WRITE;
+/*!40000 ALTER TABLE `geo` DISABLE KEYS */;
+INSERT INTO `geo` VALUES (1,59.851199,30.321108,1,'2016-11-17 00:15:58','PC'),(2,59.853382,30.321091,1,'2016-11-17 00:16:55','PC'),(3,59.85352,30.329599,1,'2016-11-17 00:17:21','PC');
+/*!40000 ALTER TABLE `geo` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
--- Структура таблицы `messages`
+-- Table structure for table `messages`
 --
 
+DROP TABLE IF EXISTS `messages`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `messages` (
-  `id` int(11) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `message` varchar(1024) NOT NULL,
   `datemessage` datetime NOT NULL,
   `isread` tinyint(1) NOT NULL,
   `idfrom` int(11) NOT NULL,
-  `idto` int(11) NOT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+  `idto` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `idfrom` (`idfrom`),
+  KEY `idto` (`idto`)
+) ENGINE=MyISAM AUTO_INCREMENT=10 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Дамп данных таблицы `messages`
+-- Dumping data for table `messages`
 --
 
-INSERT INTO `messages` (`id`, `message`, `datemessage`, `isread`, `idfrom`, `idto`) VALUES
-(1, 'xxx', '2016-11-08 00:00:00', 0, 1, 2),
-(2, 'xxx', '2016-11-08 00:00:00', 0, 1, 2),
-(3, 'xxx', '2016-11-08 00:00:00', 0, 1, 2),
-(4, 'xxx', '2016-11-08 00:00:00', 1, 2, 1),
-(5, 'xxx', '2016-11-08 00:00:00', 1, 2, 1),
-(6, 'xxxqwd', '2016-11-08 00:00:00', 1, 2, 1),
-(7, 'xxx', '2016-11-08 00:00:00', 0, 1, 2),
-(8, 'aaa', '2016-11-08 00:00:00', 1, 2, 1),
-(9, 'bbb', '2016-11-08 00:00:00', 0, 1, 2);
-
--- --------------------------------------------------------
+LOCK TABLES `messages` WRITE;
+/*!40000 ALTER TABLE `messages` DISABLE KEYS */;
+INSERT INTO `messages` VALUES (1,'xxx','2016-11-08 00:00:00',0,1,2),(2,'xxx','2016-11-08 00:00:00',0,1,2),(3,'xxx','2016-11-08 00:00:00',0,1,2),(4,'xxx','2016-11-08 00:00:00',1,2,1),(5,'xxx','2016-11-08 00:00:00',1,2,1),(6,'xxxqwd','2016-11-08 00:00:00',1,2,1),(7,'xxx','2016-11-08 00:00:00',0,1,2),(8,'aaa','2016-11-08 00:00:00',1,2,1),(9,'bbb','2016-11-08 00:00:00',0,1,2);
+/*!40000 ALTER TABLE `messages` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
--- Структура таблицы `user`
+-- Table structure for table `user`
 --
 
+DROP TABLE IF EXISTS `user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `user` (
-  `id` int(11) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `login` varchar(32) NOT NULL,
   `password` varchar(32) NOT NULL,
   `email` varchar(32) DEFAULT NULL,
   `loggedIn` tinyint(1) NOT NULL DEFAULT '0',
-  `sessionId` varchar(16) DEFAULT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+  `sessionId` varchar(16) DEFAULT NULL,
+  `ref` varchar(32) DEFAULT NULL,
+  `name` varchar(32) DEFAULT NULL,
+  `surname` varchar(32) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Дамп данных таблицы `user`
+-- Dumping data for table `user`
 --
 
-INSERT INTO `user` (`id`, `login`, `password`, `email`, `loggedIn`, `sessionId`) VALUES
-(1, 'root', '63a9f0ea7bb98050796b649e85481845', 'root@root.root', 0, NULL),
-(2, 'admin', '63a9f0ea7bb98050796b649e85481845', 'admin@root.admin', 0, NULL);
-
-
-CREATE TABLE friend (
-	id int auto_increment,
-	userId int not null,
-    friendId int not null,
-    accepted tinyint(1) not null,
-    primary key(id),
-    FOREIGN KEY (userId) REFERENCES user (id),
-    FOREIGN KEY (friendId) REFERENCES user (id)
-);
+LOCK TABLES `user` WRITE;
+/*!40000 ALTER TABLE `user` DISABLE KEYS */;
+INSERT INTO `user` VALUES (1,'root','63a9f0ea7bb98050796b649e85481845','root@root.root',0,NULL,'YlK+sv@9y68?t1o7(t7LPdbft!8#Hb_s','rnd','toor'),(2,'admin','63a9f0ea7bb98050796b649e85481845','admin@root.admin',0,NULL,'1bjnObjO4Zu)kRcTgNQ8(PskoXDF!ize','rnd','nimda'),(3,'test','098f6bcd4621d373cade4e832627b4f6','test@test.te',0,NULL,'_-BYU%SIYa9F-srT)9q_6rfza4WzVScZ','rnd','tset');
+/*!40000 ALTER TABLE `user` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
--- Поле accepted = 0 - предложение дружбы еще не приняты.
--- Поле accepted = 1 - предложение дружбы принято, пользователи стали друзьями.
-
-insert into friend (userId, friendId, accepted) values (1,2,1);
--- --------------------------------------------------------
-
---
--- Структура таблицы `xxx`
+-- Table structure for table `xxx`
 --
 
+DROP TABLE IF EXISTS `xxx`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `xxx` (
   `xxx` datetime NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Индексы сохранённых таблиц
+-- Dumping data for table `xxx`
 --
 
---
--- Индексы таблицы `geo`
---
-ALTER TABLE `geo`
-  ADD PRIMARY KEY (`id`);
+LOCK TABLES `xxx` WRITE;
+/*!40000 ALTER TABLE `xxx` DISABLE KEYS */;
+/*!40000 ALTER TABLE `xxx` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
---
--- Индексы таблицы `messages`
---
-ALTER TABLE `messages`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `idfrom` (`idfrom`),
-  ADD KEY `idto` (`idto`);
-
---
--- Индексы таблицы `user`
---
-ALTER TABLE `user`
-  ADD PRIMARY KEY (`id`);
-
---
--- AUTO_INCREMENT для сохранённых таблиц
---
-
---
--- AUTO_INCREMENT для таблицы `geo`
---
-ALTER TABLE `geo`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
---
--- AUTO_INCREMENT для таблицы `messages`
---
-ALTER TABLE `messages`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
---
--- AUTO_INCREMENT для таблицы `user`
---
-ALTER TABLE `user`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2016-11-19  3:47:02
